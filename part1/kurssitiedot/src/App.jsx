f7096e83ba28cf4dfb3e2ac57fe140150db3d261
eliastakala <etakala1998@gmail.com>
2024-01-27 16:47:23 +0200

    fixes to kurssitiedot

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,29 +1,30 @@
-const Header = (props) => {
+const Header = ({course}) => {
   return (
-      <h1>{props.course}</h1>
+      <h1>{course.name}</h1>
   )
 }
 
-const Part = (props) => {
+const Part = ({part}) => {
   return (
-      <p>{props.part.name} {props.part.exercises}</p>
+      <p>{part.name} {part.exercises}</p>
   )
 }
 
-const Content = (props) => {
+const Content = ({course}) => {
   return (
     <div>
-      <Part part = {props.parts[0]}/>
-      <Part part = {props.parts[1]}/>
-      <Part part = {props.parts[2]}/>
+      <Part part = {course.parts[0]}/>
+      <Part part = {course.parts[1]}/>
+      <Part part = {course.parts[2]}/>
     </div>
   )
 }
 
-const Total = (props) => {
+const Total = ({course}) => {
   return (
     <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>
+        Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
     </div>
   )
 }
@@ -50,9 +51,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
